775013730a30009f9a356109958b380247f904ec
MiraMai <mira.aeridou@gmail.com>
2017-03-29 20:52:31 +0200

    show chat 2

--- firebase.js
+++ firebase.js
@@ -76,5 +76,39 @@ window.addEventListener('load', function() { // Windows load
                   })
               };
             loginbtn.addEventListener('click', loginFunction);
+    
+    
+//******************* authentication ********************    
+   let provider = new firebase.auth.GithubAuthProvider();  
+    
+             firebase.auth().signInWithPopup(provider)
+                 .then(function(result) {
+	             // Om autentisering lyckas, så finns användarinfo i user
+	              let user = result.user;
+                 console.log(user);
+                 console.log(user.email);
+                 console.log(user.displayName);
+                 console.log(user.photoURL);
+                 
+                let img = document.createElement('img');
+                img.setAttribute('src', user.photoURL);
+                loggedinName.appendChild(img);
+                 
+                
+                loggedinName.innerHTML = "Logged in as " + user.email;
+                usernameInput.style.display = "none";
+                githbLogin.style.display = "none";
+                loginbtn.style.display = "none";
+                or.style.display = "none";
+                logoutbtn.style.display = "inline";
+                message.style.display = "inline";
+                send.style.display = "inline";
+                 
+                 loginFunction();
+                 
+                 });
+    
+});
+//************************************************
 
                 });// windows load
